e1a219a754cd4558e3ece2e076adf602743c7926
Rachel Davies <rachel.davies@snyk.io>
2025-02-18 21:28:51 +0000

    feat: resolve first dependencies
    - wip - same result as node but failing tests
    
    make consistent with node and golang code review exercise

--- npm_deps/package.py
+++ npm_deps/package.py
@@ -1,10 +1,18 @@
+from nodesemver import max_satisfying
+
 from npm_deps.error import PackageVersionNotFoundError
 from npm_deps.models import NPMPackage, NPMPackageVersion
 from npm_deps.package_request import request_package
 
 
 async def get_package_version(name: str, version: str) -> NPMPackageVersion:
+    """
+    Resolves the direct dependencies of an NPM package with a given name and version.
 
+    Keyword arguments:
+    name -- the name of the package
+    version -- version of the package
+    """
     package_json = await request_package(name)
 
     npm_package = NPMPackage(
@@ -16,8 +24,23 @@ async def get_package_version(name: str, version: str) -> NPMPackageVersion:
     if package_version is None:
         raise PackageVersionNotFoundError(f"Package {name} version {version} not found")
 
+    dependencies = package_version.dependencies
+    if dependencies is None:
+        return NPMPackageVersion(
+            name=name,
+            version=version,
+            dependencies=None,
+        )
+
+    resolved_dependencies = {}
+    for dependency_name, dependency_range in dependencies.items():
+        dependency_package_json = await request_package(dependency_name)
+        dependency_versions = list(dependency_package_json["versions"].keys())
+        max_satisfying_version = max_satisfying(dependency_versions, dependency_range)
+        resolved_dependencies[dependency_name] = max_satisfying_version
+
     return NPMPackageVersion(
         name=name,
         version=version,
-        dependencies=package_version.dependencies,
+        dependencies=resolved_dependencies,
     )
